a7d48d5a611a5b3d3b33e82ac54b0a8b60599ccc
Habib Santoso <habib.santoso.ft18@mail.umy.ac.id>
2022-11-04 13:50:21 +0700

    menambahkan function untuk melakukan input data

--- SortAlgorithms/Program.cs
+++ SortAlgorithms/Program.cs
@@ -13,6 +13,37 @@ namespace SortAlgorithms
         private int mov_count = 0;
 
         private int n;
+
+        void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of element in the array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("----------------------------");
+                    Console.WriteLine("Masukan elemen array");
+                    Console.WriteLine("----------------------------");
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        Console.Write("<" + (i + 1) + "> ");
+                        string s1 = Console.ReadLine();
+                        arr[i] = Int32.Parse(s1);
+                    }
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\nArray can have maximum 20 elements \n");
+                }
+            }
+        }
+
+
         static void Main(string[] args)
         {
         }
